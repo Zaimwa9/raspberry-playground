e8f70e3e0dd37d996a70663df1626d3bb0061502
wadii <wadii.zaim@essec.edu>
2018-10-02 11:07:09 +0000

    first intent working

--- action-colorChange.js
+++ action-colorChange.js
@@ -8,24 +8,24 @@ fs.writeFile("/home/pi/test", "testets");
 client.on('connect', function () {
 	console.log('connected to ' + HOST);
 	client.subscribe('hermes/hotword/default/detected');
+	client.subscribe('hermes/intent/#');
 });
 
 client.on('message', function (topic, message) {
-	if (topic == 'hermes/hotword/default/detected') {
-		console.log('message received ' + message)
+	console.log(topic);
+	console.log('message received ' + topic)
+	if (topic == 'hermes/intent/wzaim:colorChange') {
+		var x = 0;
+		var y = 0;
+		var off = [15, 150, 215];
+		while (x < 8) {	
+			y = 0;
+			while (y < 8) {
+				off = [Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255)];
+				matrix.setPixel(x, y, off);
+				y++;
+			}
+			x++;
+		}
 	}
 })
-
-var x = 0;
-var y = 0;
-var off = [15, 150, 215];
-console.log(matrix);
-while (x < 8) {	
-	y = 0;
-	while (y < 8) {
-		off = [Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255)];
-		matrix.setPixel(x, y, off);
-		y++;
-	}
-	x++;
-}
